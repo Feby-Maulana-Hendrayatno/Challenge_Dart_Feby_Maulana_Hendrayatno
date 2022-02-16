3389ad420abf8915fa0b66b26caf38738e585e46
Feby-Maulana-Hendrayatno <hendrayatno26@gmail.com>
2022-02-16 21:56:28 +0700

    Tugas Done

---
+++ Challenge_Dart_Feby_Maulana_Hendrayatno.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+void main() {
+  print("Masukan Jumlah Mata Kuliah :");
+  String? jumlah = stdin.readLineSync();
+
+  var jumlahInt = int.parse(jumlah!);
+
+  List<String> listMK = <String>[];
+
+  for (var i = 1; i <= jumlahInt; i++) {
+    print("Masukan Jumlah Mata Kuliah ke-$i:");
+    String? mk = stdin.readLineSync();
+    listMK.add(mk.toString());
+  }
+
+  for (var nm_matkul in listMK) {
+    print("Nama Mata Kuliah $nm_matkul");
+  }
+
+  print("Total Matakuliah yang di Pelajari Sebanyak $jumlahInt Matakuliah");
+}
